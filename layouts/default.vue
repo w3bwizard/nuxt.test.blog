acfa342c55624bc73562fe5733a6aabd0536c892
w3bwizard <w3bwiz4rd@gmail.com>
2022-02-03 15:16:28 +0200

    clear nuxt-blog project

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navbar />
+    <div class="container">
+      <nuxt />
+    </div>
+  </div>  
+</template>
+
+
+<script>
+import navbar from '../components/navbar'
+
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="text-center p-3">
+    <h1 class="text-danger">Error 404</h1>
+    <h2>Page not found</h2>
+    <nuxt-link to="/">Go home</nuxt-link>
+  </section>
+</template>
+
+<style scoped>
+  h1 {
+    font-weight: bolder;
+  }
+</style>
