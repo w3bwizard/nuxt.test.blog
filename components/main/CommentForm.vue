7e9fac7eb644daf578801a88b02ce36915a39e1b
w3bwizard <w3bwiz4rd@gmail.com>
2022-02-20 17:29:07 +0200

    add comments form with client validation

--- components/main/CommentForm.vue
+++ components/main/CommentForm.vue
@@ -1,20 +1,25 @@
 <template>
   <el-form
-    :model="ruleForm"
+    :model="controls"
     :rules="rules"
-    ref="ruleForm"
-    label-width="120px"
-    class="demo-ruleForm"
+    ref="form"
+    @submit.native.prevent="onSubmit"
   >
+    <b>Добавить комментарий</b>
     <el-form-item label="Ник:" prop="name">
-      <el-input v-model="ruleForm.name"></el-input>
+      <el-input v-model.trim="controls.name" />
     </el-form-item>
-    <el-form-item label="Комментарий:" prop="desc">
-      <el-input type="textarea" v-model="ruleForm.text"></el-input>
+    <el-form-item label="Комментарий:" prop="text">
+      <el-input
+        type="textarea"
+        v-model="controls.text"
+        resize="none"
+        :rows="2"
+      />
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm('ruleForm')"
-        >Create</el-button
+      <el-button type="primary" native-type="submit" @click="onSubmit()"
+        >Комментировать</el-button
       >
     </el-form-item>
   </el-form>
@@ -24,7 +29,7 @@
 export default {
   data() {
     return {
-      ruleForm: {
+      controls: {
         name: "",
         text: "",
       },
@@ -45,6 +50,18 @@ export default {
         ],
       },
     };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          console.log("Коммент отправлен");
+        } else {
+          console.log("Ошибка валидации");
+          return false;
+        }
+      });
+    },
   },
 };
 </script>
--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,30 +1,26 @@
 <template>
   <el-card class="post" :body-style="{ padding: '0px' }" shadow="hover">
-    <header class="post__header" slot="header" >
+    <header class="post__header" slot="header">
       <h1 class="post__title">
-        <a href="/post/" @click.prevent="openPost()">Nice burger</a>
+        <a href="/post/" @click.prevent="openPost()">Post Title</a>
       </h1>
-      <small><span class="el-icon-time"></span>{{ new Date().toLocaleDateString() }}</small>
+      <small
+        ><span class="el-icon-time"></span
+        >{{ new Date().toLocaleDateString() }}</small
+      >
     </header>
     <div class="post__poster">
-      <img 
-        class="post__img" 
-        src="http://cheats-by-cs.ru/_ld/0/61.jpg"
-      >
+      <img class="post__img" src="http://cheats-by-cs.ru/_ld/0/61.jpg" />
     </div>
-    
-    <div style="padding: 14px;">
+
+    <div style="padding: 14px">
       <footer class="post__footer">
-        <el-button 
-          @click.prevent="openPost()" 
-          plain
-          >Подробнее
-        </el-button>
-        <el-button 
+        <el-button @click.prevent="openPost()" plain>Подробнее </el-button>
+        <el-button
           @click.prevent="openPost()"
-          class="button" 
-          type="success" 
-          plain 
+          class="button"
+          type="success"
+          plain
           >Скачать чит
         </el-button>
       </footer>
@@ -43,40 +39,40 @@
 export default {
   methods: {
     openPost() {
-      const id = 'mega-post'
-      this.$router.push(`/post/${id}`)
-    }
-  } 
-}
+      const id = "mega-post";
+      this.$router.push(`/post/${id}`);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .post {
-    margin-bottom: 2rem;
-  }
-  .post__header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .post__title a {
-    text-decoration: none;
-    color: #353535;
-    transition: color .3s linear;
-  }
-  .post__title a:hover {
-    color: black;
-  }    
-  .post__poster {
-    display: flex;
-    padding-top: 1rem;
-  }
-  .post__img {
-    margin: auto;
-    display: block;
-  }
-  .post__footer {
-    display: flex;
-    justify-content: flex-end;
-  }
+.post {
+  margin-bottom: 2rem;
+}
+.post__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.post__title a {
+  text-decoration: none;
+  color: #353535;
+  transition: color 0.3s linear;
+}
+.post__title a:hover {
+  color: black;
+}
+.post__poster {
+  display: flex;
+  padding-top: 1rem;
+}
+.post__img {
+  margin: auto;
+  display: block;
+}
+.post__footer {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
